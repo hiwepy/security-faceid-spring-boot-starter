4a9cbd68fcbafb0d41e1dc122fde37edaacb50af
wandl <hnxyhcwdl1003@163.com>
2020-06-24 11:12:05 +0800

    代码优化

--- src/main/java/org/springframework/security/boot/SecurityFaceIDFilterConfiguration.java
+++ src/main/java/org/springframework/security/boot/SecurityFaceIDFilterConfiguration.java
@@ -47,7 +47,7 @@ public class SecurityFaceIDFilterConfiguration implements ApplicationEventPublis
 	@Configuration
 	@EnableConfigurationProperties({ SecurityFaceIDProperties.class, SecurityBizProperties.class })
 	@Order(SecurityProperties.DEFAULT_FILTER_ORDER + 3)
-	static class FaceIDWebSecurityConfigurerAdapter extends SecurityBizConfigurerAdapter {
+	static class FaceIDWebSecurityConfigurerAdapter extends AbstractSecurityConfigurerAdapter {
 
 	    private final SecurityOpenIDAuthcProperties authcProperties;
 	    
