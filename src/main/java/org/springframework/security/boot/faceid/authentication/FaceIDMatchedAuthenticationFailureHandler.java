0d7df4ffe9641a021e50ff67f02a7cc6be1d5257
wandl <hiwepy@163.com>
2026-05-27 11:06:20 +0800

    chore(pom): align to Spring Boot 4.1.x (JDK 17, Boot 4.1.0-M4)

--- src/main/java/org/springframework/security/boot/faceid/authentication/FaceIDMatchedAuthenticationFailureHandler.java
+++ src/main/java/org/springframework/security/boot/faceid/authentication/FaceIDMatchedAuthenticationFailureHandler.java
@@ -3,9 +3,9 @@ package org.springframework.security.boot.faceid.authentication;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 import org.springframework.context.support.MessageSourceAccessor;
 import org.springframework.http.HttpStatus;
