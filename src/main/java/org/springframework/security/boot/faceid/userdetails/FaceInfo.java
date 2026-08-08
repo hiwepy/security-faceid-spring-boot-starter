8700c2fa9abc495caba9746f8c49aa9acb622357
wandl-6A72h <hiwepy@gmail.com>
2026-08-09 00:00:39 +0800

    chore: propagate pom standardization, tests, javadoc and @author fix from 4.1.x
    - Add jacoco-maven-plugin 0.8.15 with 90% coverage gate
    - Add central-publishing-maven-plugin 0.11.0
    - Update surefire (enable tests, argLine for JaCoCo, *_Test.java pattern)
    - Fix SCM URLs
    - Copy test files from 4.1.x branch
    - Copy Javadoc improvements from 4.1.x branch
    - Standardize @author to [@Loong Wan](https://github.com/loong10k)
    - Propagated to 4.0.x branch

--- src/main/java/org/springframework/security/boot/faceid/userdetails/FaceInfo.java
+++ src/main/java/org/springframework/security/boot/faceid/userdetails/FaceInfo.java
@@ -19,7 +19,7 @@ import lombok.Data;
 
 /**
  * TODO
- * @author 		： <a href="https://github.com/hiwepy">wandl</a>
+ * @author [@Loong Wan](https://github.com/loong10k)
  */
 @Data
 public class FaceInfo {
