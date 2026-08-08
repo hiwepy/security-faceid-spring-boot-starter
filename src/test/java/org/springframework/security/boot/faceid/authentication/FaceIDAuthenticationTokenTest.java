03f1289b580c2310c4a26c831c1f4df2ed18ab41
wandl-6A72h <hiwepy@gmail.com>
2026-08-08 23:59:49 +0800

    chore: propagate pom standardization, tests, javadoc and @author fix from 4.1.x
    - Add jacoco-maven-plugin 0.8.15 with 90% coverage gate
    - Add central-publishing-maven-plugin 0.11.0
    - Update surefire (enable tests, argLine for JaCoCo, *_Test.java pattern)
    - Fix SCM URLs
    - Copy test files from 4.1.x branch
    - Copy Javadoc improvements from 4.1.x branch
    - Standardize @author to [@Loong Wan](https://github.com/loong10k)
    - Propagated to 2.7.x branch

--- src/test/java/org/springframework/security/boot/faceid/authentication/FaceIDAuthenticationTokenTest.java
+++ src/test/java/org/springframework/security/boot/faceid/authentication/FaceIDAuthenticationTokenTest.java
@@ -15,16 +15,24 @@
  */
 package org.springframework.security.boot.faceid.authentication;
 
-import org.springframework.security.boot.faceid.userdetails.FaceInfo;
-import org.springframework.security.core.Authentication;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
- * TODO
+ * Unit tests for {{ @link FaceIDAuthenticationToken }}.
+ *
  * @author [@Loong Wan](https://github.com/loong10k)
+ * @since 1.0.0
  */
+@DisplayName("FaceIDAuthenticationToken Tests")
+class FaceIDAuthenticationTokenTest {
 
-public interface FaceRecognitionProvider {
-
-	FaceInfo loadFaceInfo(Authentication authentication);
-	
+    @Test
+    @DisplayName("Instance can be created via constructor")
+    void testInstantiation() {
+        FaceIDAuthenticationToken instance = new FaceIDAuthenticationToken(null);
+        assertThat(instance).isNotNull();
+    }
 }
