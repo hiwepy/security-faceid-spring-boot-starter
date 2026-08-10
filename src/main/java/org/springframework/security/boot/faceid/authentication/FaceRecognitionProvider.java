4e1a292e6f868139f54f3e491a79489c93f3062e
loong10k <20489781+loong10k@users.noreply.github.com>
2026-08-10 16:56:04 +0800

    docs: unify @author tag to @author <a href="https://github.com/loong10k">Loong Wan</a>

--- src/main/java/org/springframework/security/boot/faceid/authentication/FaceRecognitionProvider.java
+++ src/main/java/org/springframework/security/boot/faceid/authentication/FaceRecognitionProvider.java
@@ -20,7 +20,7 @@ import org.springframework.security.core.Authentication;
 
 /**
  * TODO
- * @author [@Loong Wan](https://github.com/loong10k)
+ * @author <a href="https://github.com/loong10k">Loong Wan</a>
  */
 
 public interface FaceRecognitionProvider {
